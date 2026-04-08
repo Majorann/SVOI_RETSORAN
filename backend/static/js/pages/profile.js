fbcade9a3200e145fec0d2cb5e0770a7801368e4
Karhtak <fleksoff277@gmail.com>
2026-04-08 13:59:27 +0200

    Bust profile mask cache

--- backend/static/js/pages/profile.js
+++ backend/static/js/pages/profile.js
@@ -1,7 +1,7 @@
-import { setupFormEnhancements } from "../modules/formEnhancements.js";
-import { setupPaymentAddAccordion } from "../modules/paymentAddAccordion.js";
-import { setupProfileNameFit } from "../modules/profileNameFit.js";
-import { bootstrapPage } from "../shared/basePage.js";
+import { setupFormEnhancements } from "../modules/formEnhancements.js?v=20260408b";
+import { setupPaymentAddAccordion } from "../modules/paymentAddAccordion.js?v=20260408b";
+import { setupProfileNameFit } from "../modules/profileNameFit.js?v=20260408b";
+import { bootstrapPage } from "../shared/basePage.js?v=20260408b";
 
 bootstrapPage(async () => {
   setupPaymentAddAccordion();
